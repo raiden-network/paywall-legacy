f9b882a5bb32691ef9ef069ceabd8a6eed9593e9
manuelwedler <manuel.wedler@googlemail.com>
2020-10-08 09:27:09 +0200

    Add fonts to webapp

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import './main.scss';
 
 import {
   RaidenPaywallPlugin,
@@ -8,14 +9,14 @@ import {
 
 Vue.config.productionTip = false;
 
-const raiden_options = {
+const raidenOptions = {
   raidenUrl: new URL('http://lightclient.raiden.network/staging/'),
   pollInitialWaitTime: 10_000,
   pollInterval: 3_000,
   pollMaxWaitTime: 120_000,
 } as RaidenPaywallOptions;
 
-Vue.use(RaidenPaywallPlugin, raiden_options);
+Vue.use(RaidenPaywallPlugin, raidenOptions);
 
 new Vue({
   render: (h) => h(App),
