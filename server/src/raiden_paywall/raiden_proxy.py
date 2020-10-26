767e104a799dd4e58527ed5ba158c9bfe8e4d253
Maximilian <maxdacjs@gmail.com>
2020-10-26 09:55:30 +0100

    server: Wait for Raiden API on startup

--- server/src/raiden_paywall/raiden_proxy.py
+++ server/src/raiden_paywall/raiden_proxy.py
@@ -11,18 +11,42 @@ import requests
 import structlog
 from raiden.api.v1.encoding import PaymentSchema
 
-# LOG = structlog.get_logger()
+LOG = structlog.get_logger()
 
 
 class RaidenNode:
     def __init__(self, endpoint, token_address):
         self._base_url = endpoint + "/api/v1"
         self.token_address = token_address
+        self.wait_for_raiden_api()
         self.address = self._get_raiden_address()
+
+
+
+    def wait_for_raiden_api(self):
+        return asyncio.wait(self._wait_for_raiden_api, num_retries=20)
+
+
+    async def _wait_for_raiden_api(self, num_retries=20):
+        counter = 0
+        # TODO make retries and wait time parameters
+        # make this exponential backoff
+        while counter < num_retries:
+            response = requests.get(f"{self._base_url}/info")
+            if response.status_code == 200:
+                break
+            elif response.status_code == 503:
+                LOG.info(f"Raiden Node is starting up, waiting for Raiden API to be available.\
+                         Remaining retries: {num_retries - counter + 1}")
+                await asyncio.sleep(2)
+                counter += 1
+                continue
+            else:
+                response.raise_for_status()
+
 
     def _get_raiden_address(self):
         response = requests.get(f"{self._base_url}/address")
-        # TODO error handling
         return response.json()["our_address"]
 
     def get_payments(self):
